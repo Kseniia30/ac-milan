81457a92de8fe07b7f1ef46f94d951108a57e4b1
Kseniia30 <kellyshikova3004@gmail.com>
2022-09-20 19:41:58 +0200

    add

--- src/js/fetchF.js
+++ src/js/fetchF.js
@@ -3,11 +3,15 @@ const namesOfCountry = document.querySelectorAll('.name-of-country')
 function fetchFlag() {
     namesOfCountry.forEach(name => {
         fetch(`https://restcountries.com/v3.1/name/${name.textContent}?field=flags`)
-            .then(res => res.json())
+            .then(res => {
+                if (res.status === 404) {
+                    return Promise.reject(new Error());
+                } return res.json()
+            })
             .then(data => {
                 data.map(({ flags }) => {
                     name.innerHTML = `<img src="${flags.png}" alt="flag" width="40" height="20" style="margin-right:10px">${name.textContent}`
-                })
+                }).join("")
             })
             .catch(error => console.log(error))
     })
